3475a7c3b409224a45308e0d8e87047b3d86baf5
ksanjiv0005 <ksanjiv0005@gmail.com>
2022-02-10 22:26:36 +0530

    console remove in production

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   env: {
+//     devlopment: {
+//       plugins: ['transform-remove-console'], //removing consoles.log from app during release (production) versions
+//     },
+//   },
+// };
+
+module.exports = function (api) {
+  // api.cache(true);
+
+  const babelEnv = api.env();
+  const plugins = [];
+
+  if (babelEnv === 'production') {
+    plugins.push(['transform-remove-console', {exclude: ['error']}]);
+  }
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins,
+  };
 };
